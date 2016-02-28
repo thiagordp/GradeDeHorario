03f4074b31ad1c3ba63dcc8425d3a8459aaa8c6f
Thiago Raulino Dal Pont <thiago.dalpont@outlook.com>
2016-02-28 01:15:24 -0300

    Célula - exclusão de itens desnecessários

--- Modelos/Celula.cs
+++ Modelos/Celula.cs
@@ -9,22 +9,6 @@ namespace Modelos
 {
     public class Celula
     {
-
-        /// <summary>
-        /// Constante para critério de comparação na lista de alterações por Disciplina e Turma.
-        /// </summary>
-        public const int COMPARA_DISCIPLINA_TURMA = 1;
-
-        /// <summary>
-        /// Constante para critério de comparação na lista de alterações por Professor.
-        /// </summary>
-        public const int COMPARA_PROFESSOR = 2;
-
-        /// <summary>
-        /// Constante para critério de comparação na lista de alterações por Sala.
-        /// </summary>
-        public const int COMPARA_SALA = 3;
-
         public int hora { get; set; }
 
         public int dia { get; set; }
@@ -46,30 +30,6 @@ namespace Modelos
             hora = dia = fase = semestre = 0;
             disciplina = espaco = string.Empty;
             professores = new List<int>();
-        }
-
-        public bool Equal(Celula obj, int operacao)
-        {
-            switch (operacao)
-            {
-                case COMPARA_DISCIPLINA_TURMA:
-                    if ((this.disciplina != obj.disciplina) || (this.turma != obj.turma)) { return false; }
-
-                    break;
-                case COMPARA_PROFESSOR:
-
-                    for (int i = 0; i < obj.professores.Count; i++)
-                    {
-                        if (this.professores.ElementAt(i) != obj.professores.ElementAt(i)) { return false; }
-                    }
-
-                    break;
-                case COMPARA_SALA:
-                    if (this.espaco != obj.espaco) { return false; }
-
-                    break;
-            }
-            return true;
         }
     }
 }
